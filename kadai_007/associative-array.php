031492144e77e78a1f6b9690d9180c5b6c9241b6
ota-yoko <biz.ota.yoko@gmail.com>
2025-08-01 21:28:23 +0900

    「kadai_007」の課題提出物を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 配列に値を代入する
+       $vege_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($vege_data);
+       
+       ?>
+   </p>
+</body>
+
+</html>
